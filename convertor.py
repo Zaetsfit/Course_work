3bfcbef4a830a2bdbe89b04affc62921fd073ce5
Zaetsfit <jrixjf@gmail.com>
2020-05-28 09:58:53 +0300

    Check the user input

--- convertor.py
+++ convertor.py
@@ -1,12 +1,10 @@
-class Convertor:
+class Convertor(object):
     def convert(sequence):
-        correct = []
-        printable = '1234567890'
-        nums = ''
-        for token in sequence:
-            if token in printable:
-                nums += token
-            elif nums:
-                correct.append(int(nums))
-                nums = ''
-        return correct
+        big_letters = [chr(letter + 65) for letter in range(26)]
+        small_letters = [chr(letter + 97) for letter in range(26)]
+        symbols = '}{!@#$%^&(/)-_+=;:]['
+        for element in sequence:
+            if element in big_letters or element in small_letters or element in symbols:
+                return False
+        numbers_sequence = list(map(float, sequence.split(',')))
+        return numbers_sequence
